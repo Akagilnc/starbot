d6a367dff87cf313d39d243c5e2402b29b671289
DeronLee <akagilnc@gmail.com>
2016-03-24 12:52:19 +0800

    refactory 10

--- src/utils/message.js
+++ src/utils/message.js
@@ -2,27 +2,28 @@
  * Created by deronlee on 3/24/16.
  */
 
+var text = `beep boop: I hear you loud and clear! I am still learning to say more words XD`;
 var bot_name = `mikhail`
 var hello = `hello. `
 var brazil = "Oh... I know a few things about Brazil.";
 function makeMessage(msg, username) {
-    var text = `beep boop: I hear you loud and clear! I am still learning to say more words XD`;
+
 
     if (arguments == null || arguments.length == 0 || arguments[0] == null){
         return text;
     }
 
-    arguments[0] = arguments[0].toString().toLowerCase();
+    var checkMsg = arguments[0].toString().toLowerCase();
 
-    if (arguments[0] != null && arguments[0].indexOf("hello" , 11) != -1) {
+    if (checkMsg.indexOf("hello" , 11) != -1) {
         text = `I am ` + bot_name
     }
 
-    if (arguments[0] != null && arguments[0].indexOf("brazil" , 11) != -1) {
+    if (checkMsg.indexOf("brazil" , 11) != -1) {
         text = brazil
     }
 
-    text = hello + arguments[1] + " You said \'" + arguments[0] + "\'. \n " + text
+    text = hello + arguments[1] + " You said \'" + msg + "\'. \n " + text
 
     return text;
 }
