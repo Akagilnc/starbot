c4f356ff00bb0892d243cbe826c2e91b54c76af3
DeronLee <akagilnc@gmail.com>
2016-03-25 14:51:51 +0800

    add test method 8

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,9 @@
  * Created by deronlee on 3/25/16.
  */
 
-var combiner = require('stream-combiner2');
-var uglify = require('gulp-uglify');
 var gulp = require('gulp');
 
-gulp.task('test', function() {
-    var combined = combiner.obj([
-        gulp.src('bootstrap/js/*.js'),
-        uglify(),
-        gulp.dest('public/bootstrap')
-    ]);
-
-    // 任何在上面的 stream 中发生的错误，都不会抛出，
-    // 而是会被监听器捕获
-    combined.on('error', console.error.bind(console));
-
-    return combined;
+gulp.task('default', function() {
+    // place code for your default task here
 });
+
